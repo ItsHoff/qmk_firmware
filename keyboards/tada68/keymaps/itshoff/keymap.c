ab7e49cc2aa2961ce252aab9073881946d420ce1
ItsHoff <ItsHoff@users.noreply.github.com>
2018-01-20 13:18:41 +0200

    Implement macros to input special characters

--- keyboards/tada68/keymaps/itshoff/keymap.c
+++ keyboards/tada68/keymaps/itshoff/keymap.c
@@ -10,6 +10,24 @@
 
 #define _______ KC_TRNS
 
+enum custom_keycodes {
+    MY_O = SAFE_RANGE,
+    MY_A,
+    MY_AO,
+    MY_EUR,
+    SET_INPUT_F,
+    SET_INPUT_WIN,
+    SET_INPUT_MAC,
+    SET_INPUT_LIN,
+};
+
+enum input_method {
+    INPUT_F,
+    INPUT_WIN,
+    INPUT_MAC,
+    INPUT_LIN,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
@@ -35,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |   |   |   |F21|   |     |Hme |
+   * |     |   |Up | € |   |   |   |   |   |   |   | å |   |     |Hme |
    * |----------------------------------------------------------------|
-   * |      |<- |Dn | ->|   |   |Lft|Dwn|Up|Rght|F23|F22|   F24  |End |
+   * |      |<- |Dn | ->|   |   |Lft|Dwn|Up|Rght| ö | ä |   F24  |End |
    * |----------------------------------------------------------------|
    * |        |   |   |Bl-|BL |BL+|   |VU-|VU+|MUT|   |   McL|MsU|McR |
    * |----------------------------------------------------------------|
@@ -46,14 +64,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_CL] = KEYMAP_ANSI(
   _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
-  _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______, KC_F21,_______, _______,KC_HOME, \
-  _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT, KC_F23, KC_F22,    KC_F24  , KC_END, \
+  _______,_______, KC_UP, MY_EUR,_______, _______,_______,_______,_______,_______,_______, MY_AO,_______, _______,KC_HOME, \
+  _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT, MY_O, MY_A,    KC_F24  , KC_END, \
   _______,_______,_______,BL_DEC, BL_TOGG,BL_INC, _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
   _______,_______,_______,                 _______,               _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
-   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+   * |   |F|WIN |MAC |LIN |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
    * |     |   |Up |   |   |   |   |   |   |   |   |   |   |     |Hme |
    * |----------------------------------------------------------------|
@@ -65,9 +83,106 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
-  _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
+  _______,SET_INPUT_F,SET_INPUT_WIN,SET_INPUT_MAC,SET_INPUT_LIN, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
   _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______, _______,KC_HOME, \
   _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______,_______,_______,_______,_______,        _______,KC_END, \
   _______,_______,_______,BL_DEC, BL_TOGG,BL_INC, _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
   _______,_______,_______,                 _______,               _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
+};
+
+void my_tap(uint16_t keycode) {
+    register_code(keycode);
+    unregister_code(keycode);
+};
+
+#define WIN_ALT_CODE(a, b, c, d) { \
+    register_code(KC_LALT);        \
+    my_tap(KC_KP_##a);             \
+    my_tap(KC_KP_##b);             \
+    my_tap(KC_KP_##c);             \
+    my_tap(KC_KP_##d);             \
+    unregister_code(KC_LALT);      \
+    }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static int input_method = INPUT_F;
+    if (record->event.pressed) {
+        switch(keycode) {
+            case SET_INPUT_F:
+                input_method = INPUT_F;
+                return false;
+            case SET_INPUT_WIN:
+                input_method = INPUT_WIN;
+                return false;
+            case SET_INPUT_MAC:
+                input_method = INPUT_MAC;
+                return false;
+            case SET_INPUT_LIN:
+                input_method = INPUT_LIN;
+                return false;
+            case MY_O:
+                switch(input_method) {
+                    case INPUT_F:
+                    case INPUT_LIN:
+                    case INPUT_MAC:
+                        SEND_STRING(SS_TAP(X_F23));
+                        return false;
+                    case INPUT_WIN:
+                        if (keyboard_report->mods & MOD_BIT(KC_LSHIFT)) {
+                            unregister_code(KC_LSHIFT);
+                            WIN_ALT_CODE(0, 2, 1, 4);
+                            register_code(KC_LSHIFT);
+                        } else {
+                            WIN_ALT_CODE(0, 2, 4, 6);
+                        }
+                        return false;
+                }
+            case MY_A:
+                switch(input_method) {
+                    case INPUT_F:
+                    case INPUT_LIN:
+                    case INPUT_MAC:
+                        SEND_STRING(SS_TAP(X_F22));
+                        return false;
+                    case INPUT_WIN:
+                        if (keyboard_report->mods & MOD_BIT(KC_LSHIFT)) {
+                            unregister_code(KC_LSHIFT);
+                            WIN_ALT_CODE(0, 1, 9, 6);
+                            register_code(KC_LSHIFT);
+                        } else {
+                            WIN_ALT_CODE(0, 2, 2, 8);
+                        }
+                        return false;
+                }
+            case MY_AO:
+                switch(input_method) {
+                    case INPUT_F:
+                    case INPUT_LIN:
+                    case INPUT_MAC:
+                        SEND_STRING(SS_TAP(X_F21));
+                        return false;
+                    case INPUT_WIN:
+                        if (keyboard_report->mods & MOD_BIT(KC_LSHIFT)) {
+                            unregister_code(KC_LSHIFT);
+                            WIN_ALT_CODE(0, 1, 9, 7);
+                            register_code(KC_LSHIFT);
+                        } else {
+                            WIN_ALT_CODE(0, 2, 2, 9);
+                        }
+                        return false;
+                }
+            case MY_EUR:
+                switch(input_method) {
+                    case INPUT_F:
+                    case INPUT_LIN:
+                    case INPUT_MAC:
+                        SEND_STRING(SS_TAP(X_F20));
+                        return false;
+                    case INPUT_WIN:
+                        WIN_ALT_CODE(0, 1, 2, 8);
+                        return false;
+                }
+        }
+    }
+    return true;
 };
