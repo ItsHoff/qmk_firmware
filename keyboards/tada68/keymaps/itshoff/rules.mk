81f0dca2e051ba45e98a73cc61799a7d82da4853
ItsHoff <ItsHoff@users.noreply.github.com>
2019-05-14 15:16:57 +0300

    Clean up config and enable TAPPING_FORCE_HOLD

--- keyboards/tada68/keymaps/itshoff/rules.mk
+++ keyboards/tada68/keymaps/itshoff/rules.mk
@@ -16,7 +16,3 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 TAP_DANCE_ENABLE = yes      # Tap dance
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
