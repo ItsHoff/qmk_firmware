53dd4f360ab0a52af092d41c597c8547f6c59176
ItsHoff <ItsHoff@users.noreply.github.com>
2019-07-28 13:49:13 +0300

    Give layers longer names

--- keyboards/tada68/keymaps/itshoff/keymap.c
+++ keyboards/tada68/keymaps/itshoff/keymap.c
@@ -4,10 +4,10 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BL 0
-#define _GL 1
-#define _CL 2
-#define _FL 3
+#define BASE_LAYER 0
+#define GAME_LAYER 1
+#define CAPS_LAYER 2
+#define MEDIA_LAYER 3
 
 // Caps layer modifier
 #define CLMD(kc) LALT(LCTL(kc))
@@ -39,7 +39,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: (Base Layer) Default Layer
+  /* Base Layer: the default layer
    * ,----------------------------------------------------------------.
    * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |F5  |
    * |----------------------------------------------------------------|
@@ -49,17 +49,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|RsCaps| Up|PgDn|
    * |----------------------------------------------------------------|
-   * |Ctrl|Win |Alt |        Space          |Alt| FL|Esc |Lef|Dow|Rig |
+   * |Ctrl|Win |Alt |        Space          |Alt| ML|Esc |Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_BL] = LAYOUT_ansi(
+[BASE_LAYER] = LAYOUT_ansi(
   KC_GRV,          KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, KC_F5,   \
   ALL_T(KC_TAB),   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, KC_DEL,  \
-  LT(_CL, KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_ENT,          KC_F11,  \
+  LT(CAPS_LAYER, KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_ENT,          KC_F11,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,TD(TD_RSHIFT_CAPS),KC_UP, KC_PGDN, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                         KC_RALT,MO(_FL),KC_ESC, KC_LEFT, KC_DOWN,         KC_RGHT),
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                         KC_RALT,MO(MEDIA_LAYER),KC_ESC, KC_LEFT, KC_DOWN,         KC_RGHT),
 
-  /* Keymap _GL: (Game Layer) Block unwanted keys from base layer while gaming
+  /*  Game Layer: Block unwanted keys from base layer while gaming
    * ,----------------------------------------------------------------.
    * |    |  |   |   |   |   |   |   |   |   |   |   |   |       |    |
    * |----------------------------------------------------------------|
@@ -72,16 +72,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                       |   | FL|    |   |   |    |
    * `----------------------------------------------------------------'
    */
-[_GL] = LAYOUT_ansi(
-  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, \
-  KC_TAB,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS, \
-  MO(_CL), KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,  \
-  KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, \
-  KC_TRNS, KC_TRNS,KC_TRNS,                KC_TRNS,                        KC_TRNS, MO(_FL), KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS),
+[GAME_LAYER] = LAYOUT_ansi(
+  _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______, _______, _______,_______, \
+  KC_TAB,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______, _______,_______,_______, \
+  MO(CAPS_LAYER), _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,_______, _______,_______,  \
+  _______,         _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,_______, _______,_______,_______,_______, \
+  _______, _______,_______,                _______,                        _______, _______, _______, _______,_______,_______),
 
-  /* Keymap _CL: Caps Layer (rest modified as defined by CLMD)
+  /* Caps Layer: Convenience binds and rest of the keys modified by ctrl + alt
    * ,----------------------------------------------------------------.
-   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |    |
+   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |    |
    * |----------------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |    |
    * |----------------------------------------------------------------|
@@ -92,14 +92,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
    * `----------------------------------------------------------------'
    */
-[_CL] = LAYOUT_ansi(
+[CAPS_LAYER] = LAYOUT_ansi(
   CLMD(KC_GRV), KC_F1 ,     KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,         KC_F9,        KC_F10,        KC_F11,  KC_F12,        KC_DEL,        CLMD(KC_ESC),  \
   CLMD(KC_TAB), CLMD(KC_Q), CLMD(KC_W), CLMD(KC_E), CLMD(KC_R), CLMD(KC_T), CLMD(KC_Y), CLMD(KC_U), CLMD(KC_I),    CLMD(KC_O),   CLMD(KC_P),    CLMD(KC_LBRC), CLMD(KC_RBRC), CLMD(KC_BSLS), CLMD(KC_DEL),  \
   _______,      CLMD(KC_A), CLMD(KC_S), CLMD(KC_D), CLMD(KC_F), CLMD(KC_G), KC_LEFT,    KC_DOWN,    KC_UP,         KC_RIGHT,     CLMD(KC_SCLN), CLMD(KC_QUOT), CLMD(KC_ENT),           CLMD(KC_PGUP), \
   _______,      CLMD(KC_Z), CLMD(KC_X), CLMD(KC_C), CLMD(KC_V), CLMD(KC_B), CLMD(KC_N), CLMD(KC_M), CLMD(KC_COMM), CLMD(KC_DOT), CLMD(KC_SLSH),          KC_BTN1,       KC_MS_U,       KC_BTN2,       \
   _______,_______,_______,                 CLMD(KC_SPC),                                            _______,       _______,      _______,                KC_MS_L,       KC_MS_D,       KC_MS_R),
 
-  /* Keymap _FL: Function Layer
+  /* Media Layer: Media keys and other random keys
    * ,----------------------------------------------------------------.
    * |   |KP1|KP2|KP3|KP4|KP5|KP6|KP7|KP8|KP9|KP0|VU-|VU+|Mute   |Ins |
    * |----------------------------------------------------------------|
@@ -112,10 +112,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
    * `----------------------------------------------------------------'
    */
-[_FL] = LAYOUT_ansi(
+[MEDIA_LAYER] = LAYOUT_ansi(
   _______, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE, KC_INS ,  \
   _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______, KC_PSCREEN, KC_HOME, \
-  KC_ESC ,KC_LEFT,KC_DOWN,KC_RIGHT,_______,TG(_GL),_______,_______,_______,_______,_______,_______,        _______,KC_END, \
+  KC_ESC ,KC_LEFT,KC_DOWN,KC_RIGHT,_______,TG(GAME_LAYER),_______,_______,_______,_______,_______,_______,        _______,KC_END, \
   _______,_______,_______,BL_DEC, BL_TOGG,BL_INC, _______,_______,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,KC_BTN1, KC_MS_U, KC_BTN2, \
   _______,_______,_______,                 _______,               _______,_______,KC_F24,KC_MS_L,KC_MS_D, KC_MS_R),
 };
