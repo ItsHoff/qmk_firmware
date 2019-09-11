9d1d7891f9cb5c066bae351790a025be8305c2be
ItsHoff <ItsHoff@users.noreply.github.com>
2019-09-11 21:34:46 +0300

    Replace Hyper with Meh
    Windows does what ever it wants with windows key so don't press it.

--- keyboards/tada68/keymaps/itshoff/keymap.c
+++ keyboards/tada68/keymaps/itshoff/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |F5  |
    * |----------------------------------------------------------------|
-   * |HPR(Tab)| Q| W|  E|  R|  T|  Y|  U|  I|  O|  P|  [| ]|  \  |Del |
+   * |MEH(Tab)| Q| W|  E|  R|  T|  Y|  U|  I|  O|  P|  [| ]|  \  |Del |
    * |----------------------------------------------------------------|
    * |CL(Esc)|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |F11 |
    * |----------------------------------------------------------------|
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [BASE_LAYER] = LAYOUT_ansi(
   KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_F5, \
-  ALL_T(KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
+  MEH_T(KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
   LT(CAPS_LAYER, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_F11, \
   KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_RSHIFT_CAPS), KC_UP, KC_PGDN, \
   KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC  , MO(UNICODE_LAYER), MO(MEDIA_LAYER), RCTL_T(KC_ESC), KC_LEFT, KC_DOWN, KC_RGHT),
