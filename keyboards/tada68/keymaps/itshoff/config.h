be1a5b71b4453780fd69392197731ed558b9b827
ItsHoff <ItsHoff@users.noreply.github.com>
2020-05-16 10:27:10 +0300

    Update to newest qmk

--- keyboards/tada68/keymaps/itshoff/config.h
+++ keyboards/tada68/keymaps/itshoff/config.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../../config.h"
+
 /* Allows quick tap followed by hold modifier (like esc + caps layer) but break tapping toggles.
  * https://docs.qmk.fm/#/feature_advanced_keycodes?id=tapping-force-hold
  */
